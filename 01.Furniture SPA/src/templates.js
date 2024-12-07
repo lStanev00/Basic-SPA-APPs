6a2e037e19a8cef4b473406f4e203f70e3e85908
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2024-12-07 17:34:52 +0200

    modified:   01.Furniture SPA/src/templates.js

--- 01.Furniture SPA/src/templates.js
+++ 01.Furniture SPA/src/templates.js
@@ -225,23 +225,5 @@ export const containers =  {
         </div>
     </div>`
 })}
-
-
-
-    <!-- <div class="col-md-4">
-        <div class="card text-white bg-primary">
-            <div class="card-body">
-                    <img src="./images/table.png" />
-                    <p>Description here</p>
-                    <footer>
-                        <p>Price: <span>235 $</span></p>
-                    </footer>
-                    <div>
-                        <a href="#" class="btn btn-info">Details</a>
-                    </div>
-            </div>
-        </div>
-    </div> -->
-    
 </div>`
 }
